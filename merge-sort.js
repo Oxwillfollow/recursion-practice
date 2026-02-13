68f2c999898b07d086bef1674076353870b4ab7d
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-02-13 15:01:18 +0200

    Write tests for mergeSort

---
+++ merge-sort.js
@@ -0,0 +1,3 @@
+export const mergeSort = (arr) => {
+  return arr;
+};
---
+++ merge-sort.test.js
@@ -0,0 +1,20 @@
+import { mergeSort } from "./merge-sort";
+
+test("Empty array", () => {
+  expect(mergeSort([])).toEqual([]);
+});
+
+test("2 items", () => {
+  expect(mergeSort([4, 1])).toEqual([1, 4]);
+});
+
+test("5 items", () => {
+  expect(mergeSort([4, 1, 5, 2, 7])).toEqual([1, 2, 4, 5, 7]);
+});
+
+test("Doesn't mutate input array", () => {
+  const originalArr = [1, 3, 2];
+  const inputArr = [...inputArr];
+  mergeSort(copyArr);
+  expect(originalArr.toStrictEqual([inputArr]));
+});
