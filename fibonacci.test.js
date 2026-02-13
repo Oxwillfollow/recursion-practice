011e5279180dd4b2262e2d6a22a716f7fb9ba74e
Oxwillfollow <240659154+Oxwillfollow@users.noreply.github.com>
2026-02-13 14:47:23 +0200

    Setup new function for printing fibonacci array, write test for it

--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,21 +1,29 @@
 import { fibonacci } from "./fibonacci";
 
-test("n = 0", () => {
-  expect(fibonacci(0)).toBe(0);
+describe("fibonacci tests", () => {
+  test("n = 0", () => {
+    expect(fibonacci(0)).toBe(0);
+  });
+
+  test("n = 1", () => {
+    expect(fibonacci(1)).toBe(1);
+  });
+
+  test("n = 2", () => {
+    expect(fibonacci(2)).toBe(1);
+  });
+
+  test("n = 3", () => {
+    expect(fibonacci(3)).toBe(2);
+  });
+
+  test("n = 11", () => {
+    expect(fibonacci(11)).toBe(89);
+  });
 });
 
-test("n = 1", () => {
-  expect(fibonacci(1)).toBe(1);
-});
-
-test("n = 2", () => {
-  expect(fibonacci(2)).toBe(1);
-});
-
-test("n = 3", () => {
-  expect(fibonacci(3)).toBe(2);
-});
-
-test("n = 11", () => {
-  expect(fibonacci(11)).toBe(89);
+describe("fib tests", () => {
+  test("n = 8", () => {
+    expect(fib(8)).toBe([0, 1, 1, 2, 3, 5, 8, 13]);
+  });
 });
